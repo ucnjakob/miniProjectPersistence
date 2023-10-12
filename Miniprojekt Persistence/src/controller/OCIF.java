bb839c08610958626215e517546868f28ebcc8ad
jakob jensen <128793674+ucnjakob@users.noreply.github.com>
2023-10-12 08:52:48 +0200

    updated orderIF

--- Miniprojekt Persistence/src/controller/OCIF.java
+++ Miniprojekt Persistence/src/controller/OCIF.java
@@ -1,5 +1,14 @@
 package controller;
+import model.*;
 
 public interface OCIF {
 
+	public Staff addStaff(Staff Staff);
+	public Staff findStaff(String name);
+	public Customer findCustomer(String name);
+	public Customer addCustomer(Customer customer);
+	public Product findProduct(String name);
+	public Product addProduct(Product product);
+	public void createOrder();
+	public void finalizeOrder();
 }
--- Miniprojekt Persistence/src/controller/OrderController.java
+++ Miniprojekt Persistence/src/controller/OrderController.java
@@ -1,5 +1,9 @@
 package controller;
 import database.*;
+import model.Customer;
+import model.Product;
+import model.Staff;
+
 public class OrderController implements OCIF{
 	
 	CCIF ccif = new CustomerController();
@@ -10,6 +14,54 @@ public class OrderController implements OCIF{
 	
 	public OrderController() {
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Staff addStaff(Staff Staff) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Staff findStaff(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Customer findCustomer(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Customer addCustomer(Customer customer) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Product findProduct(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Product addProduct(Product product) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void createOrder() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void finalizeOrder() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
