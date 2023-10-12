3745536dda683fb2ea43ecd0abf625d46ba7faf3
jakob jensen <128793674+ucnjakob@users.noreply.github.com>
2023-10-12 09:34:22 +0200

    login ish

--- Miniprojekt Persistence/src/model/Staff.java
+++ Miniprojekt Persistence/src/model/Staff.java
@@ -20,4 +20,60 @@ String logInID;
 		this.logInID = logInID;
 	}
 
+	public String getFname() {
+		return fname;
+	}
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+	public String getLname() {
+		return lname;
+	}
+
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+
+	public int getPhoneNo() {
+		return phoneNo;
+	}
+
+	public void setPhoneNo(int phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPw() {
+		return pw;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+
+	public boolean isAdmin() {
+		return isAdmin;
+	}
+
+	public void setAdmin(boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+
+	public String getLogInID() {
+		return logInID;
+	}
+
+	public void setLogInID(String logInID) {
+		this.logInID = logInID;
+	}
+
 }
